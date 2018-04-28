de630eb49412f0ef25cb5a010efd6922a5c4d28b
Travis Paul <tr@vispaul.me>
2018-04-28 15:57:51 +0800

    Fix asm generation with mlcd -a

--- common.h
+++ common.h
@@ -2,14 +2,14 @@
 #define MLCD_COMMON_H
 
 #define BYTE_TO_ASCII(byte)  \
-	(byte & 0x80 ? '#' : ' '), \
-	(byte & 0x40 ? '#' : ' '), \
-	(byte & 0x20 ? '#' : ' '), \
-	(byte & 0x10 ? '#' : ' '), \
-	(byte & 0x08 ? '#' : ' '), \
-	(byte & 0x04 ? '#' : ' '), \
-	(byte & 0x02 ? '#' : ' '), \
-	(byte & 0x01 ? '#' : ' ')
+	(byte & 0x80 ? '1' : '0'), \
+	(byte & 0x40 ? '1' : '0'), \
+	(byte & 0x20 ? '1' : '0'), \
+	(byte & 0x10 ? '1' : '0'), \
+	(byte & 0x08 ? '1' : '0'), \
+	(byte & 0x04 ? '1' : '0'), \
+	(byte & 0x02 ? '1' : '0'), \
+	(byte & 0x01 ? '1' : '0')
 
 #define MLCD_WIDTH 48
 #define MLCD_HEIGHT 32
